f17c73531d31668b8fac9f7e8a8d40d6b8c00b0f
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-04-23 10:30:03 +0630

    fix for home page ui

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -10,30 +10,32 @@
                 </div>
             @endif
 
-            @foreach ($articles as $article)
-            <div class="card mb-2">
-                <div class="card-body">
-                    <h4 class="card-title">
-                        {{ $article->title }}
-                    </h4>
-                    <div class="text-muted small">
-                        <div class="badge bg-secondary">
-                            <b>Category: </b>
-                            {{ $article->category->name }}
+            <div class="row p-2">
+                @foreach ($articles as $article)
+                    <div class="col-12 col-md-4 mb-4">
+                        <div class="card-body alert alert-success w-100 h-100 p-3 rounded shadow-sm">
+                            <h3 class="card-title">
+                                {{ $article->title }}
+                            </h3>
+                            <div class="text-muted small">
+                                <div class="badge bg-danger">
+                                    <b>Category: </b>
+                                    {{ $article->category->name }}
+                                </div>
+                                <div class="badge bg-danger">
+                                    <b>Comment: {{ count($article->comments) }}</b>
+                                </div>
+                                <div class="badge bg-danger">
+                                    {{ $article->created_at->diffForHumans() }}
+                                </div>
+                            </div>
+                            <div>
+                                {{ $article->body }}
+                            </div>
+                            <a href="{{ url("/articles/detail/$article->id") }}" class="card-link">Detail</a>
                         </div>
-                        <div class="badge bg-secondary">
-                            <b>Comment: {{ count($article->comments) }}</b>
-                        </div>
-                        <div class="badge bg-secondary">
-                            {{ $article->created_at->diffForHumans() }}
-                        </div>
-                    </div>
-                    <div>
-                        {{ $article->body }}
-                    </div>
-                    <a href="{{ url("/articles/detail/$article->id") }}" class="card-link">Detail</a>
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
 @endsection
