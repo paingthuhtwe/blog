a034d1ad4c6a7df2de993a4e5f52e00615a4ae6a
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-04 19:01:53 +0630

    add api controller for category

---
+++ app/Http/Controllers/CategoryApiController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryApiController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        $validator = validator(request()->all(), [
+            'name' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response($validator->errors()->all(), 400);
+        }
+
+        $category = new Category();
+        $category->name = request()->name;
+        $category->save();
+
+        return $category;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        return $category;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id)
+    {
+        $validator = validator(request()->all(), [
+            'name' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response($validator->errors()->all(), 400);
+        }
+
+        $category = Category::find($id);
+        $category->name = request()->name;
+        $category->save();
+
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+
+        return $category;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,31 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\CategoryApiController;
+use App\Models\User;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', function () {
+    $validator = validator(request()->all(), [
+        'eamil' => 'requierd',
+        'password' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+        return response($validator->errors()->all(), 400);
+    }
+
+    $user = User::where('email', request()->email)->first();
+    if ($user) {
+        if (password_verify(request()->password, $user->password)) {
+            return $user->createToken('browser')->plainTextToken;
+        }
+    }
+
+    return response(['msg' => 'Incorrect email or password!'], 401);
+});
+
+Route::apiResource('/categories', CategoryApiController::class);
